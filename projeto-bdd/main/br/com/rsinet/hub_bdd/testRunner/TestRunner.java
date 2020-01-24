ef73abe4092b951a1a92cef22c20c454e6c0f007
lucas.correia <lucas.crvalho79@hotmail.com>
2020-01-24 16:08:40 -0300

    Extend report implementado.

--- projeto-bdd/main/br/com/rsinet/hub_bdd/testRunner/TestRunner.java
+++ projeto-bdd/main/br/com/rsinet/hub_bdd/testRunner/TestRunner.java
@@ -8,28 +8,25 @@ import org.junit.runner.RunWith;
 
 import com.cucumber.listener.Reporter;
 
-import br.com.rsinet.hub_bdd.managers.FileReaderManager;
 import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
+
+
 
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
- features = "classpath:feature",
+ features = "Feature",
  glue = {"br.com.rsinet.hub_bdd.stepDefinitions"},
- plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/resultados.html"},
- snippets = SnippetType.CAMELCASE,
- monochrome = true,
- dryRun = false,
- tags = {"@TesteDeSucesso, @TesteDeFalha"}
+ plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
+ monochrome = true
  )
 
 public class TestRunner {
 	@AfterClass
 	public static void writeExtentReport() {
-		Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+		Reporter.loadXMLConfig(new File("configs/extension-config.xml"));
 	}
 
 }
